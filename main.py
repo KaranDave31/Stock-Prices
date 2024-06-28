7d4e4c1b0a554e929c5912f158ea67a02d50dda5
Karan Dave <101795113+KaranDave31@users.noreply.github.com>
2024-06-28 22:32:27 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,13 +2,13 @@ import streamlit as st
 import pandas as pd
 import os
 import io
-import matplotlib.pyplot as plt
+import zipfile
 from plotly import graph_objs as go
 
 st.title('Stock Analysis Project')
 
 st.sidebar.title('Navigation')
-section = st.sidebar.selectbox('Go to', ['Master DataFrame Options', 'Sorting Options', 'Plotting Options', 'Other Financial Options'])
+section = st.sidebar.selectbox('Go to', ['Master DataFrame Options', 'Sorting Options', 'Plotting Options', 'Other Financial Options','Moving Average'])
 
 # File uploader
 uploaded_files = st.file_uploader("Select your CSV file/files", accept_multiple_files=True, type=["csv"])
@@ -36,6 +36,7 @@ if 'sort_column' not in st.session_state:
 
 if 'sort_column2' not in st.session_state:
         st.session_state.sort_column2 = 'OPEN'
+
 
 # Master DataFrame Options
 if section == 'Master DataFrame Options':
@@ -73,7 +74,7 @@ if section == 'Master DataFrame Options':
 
         if st.sidebar.button('Convert TIMESTAMP to datetime and set as index'):
             if 'TIMESTAMP' in st.session_state.master_df.columns:
-                st.session_state.master_df['TIMESTAMP'] = pd.to_datetime(st.session_state.master_df['TIMESTAMP'], infer_datetime_format=True)
+                st.session_state.master_df['TIMESTAMP'] = pd.to_datetime(st.session_state.master_df['TIMESTAMP'], format='mixed')
                 st.session_state.master_df = st.session_state.master_df.set_index(['TIMESTAMP'])
                 st.subheader('Master DataFrame after Converting TIMESTAMP')
                 st.write(st.session_state.master_df)
@@ -90,7 +91,7 @@ if section == 'Sorting Options' and st.session_state.master_df_created:
     start_date = st.sidebar.date_input('Start Date', value=st.session_state.start_date, key='sort_start_date')
     end_date = st.sidebar.date_input('End Date', value=st.session_state.end_date, key='sort_end_date')
 
-    
+ 
     # Update session state with selected dates
     st.session_state.start_date = start_date
     st.session_state.end_date = end_date
@@ -123,20 +124,16 @@ if section == 'Plotting Options' and st.session_state.master_df_created:
     # Series selection
     series = st.session_state.master_df['SERIES'].unique()
     selected_series1 = st.sidebar.selectbox('Select Series 1', series)
-    selected_series2 = st.sidebar.selectbox('Select Series 2', series)
-
-
-    # Filter dataframe based on selected series for symbol selection
     filtered_df1 = st.session_state.master_df[st.session_state.master_df['SERIES'] == selected_series1]
     symbols1 = filtered_df1['SYMBOL'].unique() if 'SYMBOL' in filtered_df1.columns else []
+    symbol1 = st.sidebar.selectbox('Select Symbol 1', symbols1)
 
+
+    selected_series2 = st.sidebar.selectbox('Select Series 2', series)
     filtered_df2 = st.session_state.master_df[st.session_state.master_df['SERIES'] == selected_series2]
     symbols2 = filtered_df2['SYMBOL'].unique() if 'SYMBOL' in filtered_df2.columns else []
-
-
-    # Symbol selection
-    symbol1 = st.sidebar.selectbox('Select Symbol 1', symbols1)
     symbol2 = st.sidebar.selectbox('Select Symbol 2', symbols2)
+
     selected_symbol = [symbol1, symbol2]
 
     # Update session state with selected dates
@@ -205,5 +202,98 @@ if section == 'Other Financial Options' and st.session_state.master_df_created:
         st.write(calculate_cumulative_returns(st.session_state.start_date, st.session_state.end_date,selected_symbol))
 
 
+if section == 'Moving Average' and st.session_state.master_df_created:
+    def extract_data(selected_symbol):
+        data_folder = os.path.join(os.getcwd(), 'data')
+        st.write(data_folder)
+        all_files = [file for file in os.listdir(data_folder) if file.endswith('.zip')]
+
+        df_lists = []
+        st.write(all_files)
+        total_files = len(all_files)
+        progress_bar = st.progress(0)
+        
+        for i, zip_file in enumerate(all_files):
+            with zipfile.ZipFile(os.path.join(data_folder, zip_file), 'r') as zip_ref:
+                for filename in zip_ref.namelist():
+                    # st.write(filename)
+                    if filename.endswith('_NSE.csv'):
+                        # st.write(filename)
+                        with zip_ref.open(filename) as file:
+                            df = pd.read_csv(file)
+                            df['TIMESTAMP'] = pd.to_datetime(df['TIMESTAMP'], format='mixed')
+                            df = df.set_index(['TIMESTAMP'])
+                            
+
+                            if selected_symbol in df['SYMBOL'].values:
+                                df_lists.append(df[df['SYMBOL'] == selected_symbol])
+                            # st.write(df_lists)
+                            
+            progress_bar.progress((i + 1) / total_files)
+
+        if df_lists:
+            return pd.concat(df_lists).sort_index()
+        else:
+            return pd.DataFrame()
+    
+    def calc_movingAvg(df, duration):
+        return df['CLOSE'].rolling(window=duration).mean()
+
+
+    def display_plot(Ycolumn, durations):
+        st.session_state.master_df = st.session_state.master_df[st.session_state.master_df['SYMBOL'] == symbol1]
+        st.session_state.master_df = st.session_state.master_df.sort_index()
+        st.write(st.session_state.master_df)
+
+        fig = go.Figure()
+        fig.add_trace(go.Scatter(x=st.session_state.master_df.index, y=st.session_state.master_df[Ycolumn], mode='lines', name='Actual'))
+        
+        colors = ['red', 'green', 'orange', 'purple','yellow']  # Define a list of colors for different moving averages
+
+        for i, duration in enumerate(durations):
+            moving_avg = calc_movingAvg(st.session_state.master_df, duration)
+            color = colors[i % len(colors)]  # Cycle through colors
+            fig.add_trace(go.Scatter(x=st.session_state.master_df.index, y=moving_avg, mode='lines', name=f'Moving Avg {duration} days', line=dict(color=color)))
+
+        fig.update_layout(title='Moving Average Plot', xaxis_rangeslider_visible=True)
+        st.plotly_chart(fig)
+
+
+
+
+
+
+
+
+    start_date_plot = st.sidebar.date_input('Start Date', value=st.session_state.start_date, key='plot_start_date')
+    end_date_plot = st.sidebar.date_input('End Date', value=st.session_state.end_date, key='plot_end_date')
+
+    series = st.session_state.master_df['SERIES'].unique()
+    selected_series1 = st.sidebar.selectbox('Select Series ', series)
+    filtered_df1 = st.session_state.master_df[st.session_state.master_df['SERIES'] == selected_series1]
+    symbols1 = filtered_df1['SYMBOL'].unique() if 'SYMBOL' in filtered_df1.columns else []
+    symbol1 = st.sidebar.selectbox('Select Symbol ', symbols1)
+
+    st.session_state.start_date_plot = start_date_plot
+    st.session_state.end_date_plot = end_date_plot
+    
+    duration_options = {'1 W': 5, '1 M': 22, '3 M': 66, '6 M': 132, '1 Y': 264}
+    selected_duration = st.sidebar.multiselect("Select Duration", list(duration_options.keys()))
+    # selected_duration = st.sidebar.selectbox('Select Duration', list(duration_options.keys()))
+
+    if st.sidebar.button('Extract More Data'):
+        extracted_data = extract_data(symbol1)
+        st.write(extracted_data)
+        st.session_state.master_df = pd.concat([st.session_state.master_df, extracted_data]).drop_duplicates()
+        st.write(st.session_state.master_df)
+
+    
+
+    if st.sidebar.button('Display plot'):
+        duration = [duration_options[duration] for duration in selected_duration]
+        # # duration = duration_options[selected_duration]
+        st.write(duration)
+
+        display_plot('CLOSE', duration)
 
 
